e205715831ded6684bd62383baf7317ad1413d61
thais viana <thaisviana081@outlook.com>
2022-11-08 19:37:53 -0300

    7 - Crie as validações para as pessoas usuárias ✔

--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -2,6 +2,8 @@ import UserModel from '../models/users.model';
 import { IUsers } from '../interfaces/users.interfeces';
 import HttpException from '../utils/http.exception';
 import GenerateToken from '../utils/tokenGerateNewUser';
+import validation from '../validation/validate';
+import { schemaUser } from '../validation/schema'; 
 
 export default class UserService {
   public userModel = new UserModel();
@@ -9,6 +11,7 @@ export default class UserService {
   public Token = new GenerateToken();
 
   public async createUser(user:IUsers) {
+    await validation(schemaUser, user);
     const newUser = await this.userModel.create(user);
     if (newUser === null) {
       throw new HttpException(400, 'Invalid');
